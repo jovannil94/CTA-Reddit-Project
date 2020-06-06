d433a4f8ba8a53ea4491f8cb0f8b297794a21ddf
jovannil94 <jovanniluna@pursuit.org>
2020-06-05 22:13:14 -0400

    adds navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import LogIn from './components/logIn';
+import { Route, Switch } from "react-router-dom";
+import LogIn from './components/LogIn';
+import HomePage from './components/HomePage';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <LogIn/>
+      <NavBar/>
+      <Switch>
+        <Route exact path={"/login"} component={LogIn}/>
+        <Route component={HomePage} />
+      </Switch>
     </div>
   );
 }
